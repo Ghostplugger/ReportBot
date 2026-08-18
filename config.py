d09a0d71ff1bac2611a01f6a46204deb21805b94
Ghostplugger <kanhapandit143x@gmail.com>
2026-08-18 10:33:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-API_ID = int(os.environ.get("API_ID", 0))
-API_HASH = os.environ.get("API_HASH")
-OWNER_ID = int(os.environ.get("OWNER_ID", 0))
+BOT_TOKEN = os.environ.get("8874711768:AAEKxg9vMqhrxoatpKkh0iN9lhd-N0_E8ts")
+API_ID = int(os.environ.get("38884525", 0))
+API_HASH = os.environ.get("ffad13e71c9fcff37b5fef790ae0fda8")
+OWNER_ID = int(os.environ.get("7380059056", 0))
 MONGO_URL = os.environ.get("MONGO_URL")
 DB_NAME = "startlove"
 
